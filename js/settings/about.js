eef0162ff68d6be017387b774eeb24b24e7ee695
Sett Sarverott <sarverott@gmail.com>
2019-12-10 20:18:28 +0100

    description change

--- js/settings/about.js
+++ js/settings/about.js
@@ -1,4 +1,4 @@
 var aboutSettings={
-  content:`I am programmer and IT technician. Coding is my passion. I love to finding new ways to solve the problem. Creating mechanisms on servers, on IoT electronics and in games makes me proud when they makes their work. I hope that my work will be interesting for You.`
+  content:`I'm just blacksmith, I smith many things like jewelery, electronics and software. I hope You find my work interesting.`
 };
 loadNext();
